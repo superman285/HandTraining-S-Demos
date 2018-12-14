e0a23ccacb4f7cfba38d9068ca0f650fdfd1efbe
superman285 <superman285@126.com>
2018-12-14 23:34:33 +0800

    Carevin幼儿园项目，跳转顶部，用了sticky定位；菜单动画，用了jquery的自带动画slideToggle  2018.12.14 update

---
+++ CarevinProject/js/main.js
@@ -0,0 +1,24 @@
+var menu_pages = $(".pages"),
+    submenu_pages = $(".main-submenu"),
+    down_arrow = $(".down-arrow");
+
+menu_pages.click(()=>{
+    down_arrow.toggleClass("click");
+    submenu_pages.slideToggle(400);
+
+});
+
+
+console.log(menu_pages,submenu_pages);
+
+
+
+//引入轮播
+var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal', // 垂直切换选项
+    loop: true, // 循环模式选项
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+    },
+})

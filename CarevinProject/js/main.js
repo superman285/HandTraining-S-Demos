a9ca2e3521a2c4262286017457d5ac65e7426130
superman285 <superman285@126.com>
2018-12-15 23:27:09 +0800

    Carevin幼儿园项目，轮播动画，swiper插件的使用  2018.12.15 update

--- CarevinProject/js/main.js
+++ CarevinProject/js/main.js
@@ -1,4 +1,4 @@
-var menu_pages = $(".pages"),
+let menu_pages = $(".pages"),
     submenu_pages = $(".main-submenu"),
     down_arrow = $(".down-arrow");
 
@@ -14,11 +14,32 @@ console.log(menu_pages,submenu_pages);
 
 
 //引入轮播
-var mySwiper = new Swiper ('.swiper-container', {
+let mySwiper = new Swiper ('.swiper-container.top-slide-wrap', {
     direction: 'horizontal', // 垂直切换选项
     loop: true, // 循环模式选项
+    autoplay: {
+      delay: 3000,
+      stopOnLastSlide: true,
+      disableOnInteraction: false,
+    },
     // 如果需要分页器
     pagination: {
         el: '.swiper-pagination',
+        clickable: 'true'
     },
-})
+});
+
+let mySwiper2 = new Swiper ('.swiper-container.parent-reviews', {
+    direction: 'vertical', // 垂直切换选项
+    loop: true, // 循环模式选项
+    autoplay: {
+        delay: 3000,
+        stopOnLastSlide: true,
+        disableOnInteraction: false,
+    },
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: 'true'
+    },
+});
